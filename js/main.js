99b7ffeae11d8b09981c9dd6bf15dd2c2f8cc92a
ahmeddoban <ahmedzoro323@gmail.com>
2022-09-22 18:20:56 +0200

    add setting and function it

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-back-to-top
+# back-to-top
 ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*/
 let btn = document.querySelector(".back-to-top");
 btn.addEventListener("click", (e) => {
@@ -16,21 +16,93 @@ window.onscroll = (ele) => {
     btn.style.cssText = "display : none";
   }
 };
+
 /*ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-landing bg
+# setting box
+ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*/
+let Setting_Btn = document.querySelector(".setting-box i");
+let setting_box = document.querySelector(".setting-box");
+let main_color = localStorage.getItem("color_option");
+let box_Colors = document.querySelectorAll(".box-colors li ");
+
+/********* # Setting button event # **************/
+Setting_Btn.addEventListener("click", () => {
+  setting_box.classList.toggle("open");
+  Setting_Btn.classList.toggle("fa-spin");
+});
+/********* # Check local storage # **************/
+if (main_color !== null) {
+  box_Colors.forEach((li) => {
+    li.classList.remove("active");
+    if (li.dataset.color === main_color) {
+      li.classList.add("active");
+    }
+  });
+  document.documentElement.style.setProperty("--main-color", main_color);
+}
+/********* # change local storage and active class # **************/
+box_Colors.forEach((li) => {
+  li.addEventListener("click", (each_li) => {
+    box_Colors.forEach((li) => {
+      li.classList.remove("active");
+      document.documentElement.style.setProperty(
+        "--main-color",
+        each_li.target.dataset.color
+      );
+    });
+    each_li.target.classList.add("active");
+    localStorage.setItem("color_option", each_li.target.dataset.color);
+  });
+});
+/********* # change background active class # **************/
+let random_bg_option = document.querySelectorAll(".random-background span");
+let bg_localStorage = localStorage.getItem("background_roundom");
+let backgroundOption = true;
+let BG_ClearInterval;
+
+if (bg_localStorage !== null) {
+  random_bg_option.forEach((span) => {
+    span.classList.remove("active");
+  });
+  if (bg_localStorage === "true") {
+    backgroundOption = true;
+    document.querySelector(".yes").classList.add("active");
+  } else {
+    backgroundOption = false;
+    document.querySelector(".no").classList.add("active");
+  }
+}
+
+random_bg_option.forEach((span) => {
+  span.addEventListener("click", (ele) => {
+    random_bg_option.forEach((span) => {
+      span.classList.remove("active");
+    });
+    ele.target.classList.add("active");
+    if (ele.target.dataset.background === "yes") {
+      backgroundOption = true;
+      randomImg();
+      localStorage.setItem("background_roundom", backgroundOption);
+    } else {
+      backgroundOption = false;
+      clearInterval(BG_ClearInterval);
+      localStorage.setItem("background_roundom", backgroundOption);
+    }
+  });
+});
+/*ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+# landing bg
 ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*/
 let landing = document.querySelector(".landing");
-let bg = [
-  "01.jpg",
-  "02.jpg",
-  "03.jpg",
-  "04.jpg",
-  "05.jpg",
-  "07.jpg",
-  "09.jpg",
-  "10.jpg",
-];
-setInterval(function () {
-  let random = (Math.random() * bg.length).toFixed();
-  landing.style.cssText = `background-image: url(../img/${bg[random]})`;
-}, 10000);
+let bg = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+randomImg = () => {
+  if (backgroundOption === true) {
+    BG_ClearInterval = setInterval(() => {
+      let random = Math.floor(Math.random() * bg.length);
+      landing.style.cssText = `background-image: url(../img/${bg[random]})`;
+    }, 1000);
+  }
+};
+
+randomImg();
